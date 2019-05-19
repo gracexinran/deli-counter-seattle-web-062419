7bbeff1b7aaf49666241edfaae7caa18d0fd873b
Xinran Che <gracexinran@gmail.com>
2019-05-19 21:15:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,11 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{line.inspect}"
+    line = []
+    katz_deli.length.times do |i|
+      line.push("#{i+1}. #{katz_deli[i]}")
+    end
+    puts "The line is currently: #{line.join(" ")}"
   end
 end
 
